6bd006ee097ff6e86b741d69b07e72b82471f39f
Hovhannes <hhovhannisyan348@gmail.com>
2025-07-15 21:16:16 +0400

    Добавил .gitignore и исключил node_modules

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', // обращаемся к проксированным путям
+  baseURL: import.meta.env.VITE_API_URL,  // будет браться из .env
 });
 
 api.interceptors.request.use((config) => {
